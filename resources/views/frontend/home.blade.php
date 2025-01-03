63643150ed408656397e7abeb64702cadfb6a418
HabibAroua <habib.aroua@sesame.com.tn>
2025-01-03 14:03:18 +0100

    orgnise code

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,43 +1,43 @@
 @extends('frontend.master')
 @section('content')
-<div class="container">
-    <div class="zone">
-        <h1>Tic Tac Toe</h1>
-        <form>
-            <div>
-                <input id="bt1" type="button" value=""  />
-                <input id="bt2" type="button" value=""  />
-                <input id="bt3" type="button" value=""  />
-            </div>
-            <div>
-                <input id="bt4" type="button" value="" />
-                <input id="bt5" type="button" value="" />
-                <input id="bt6" type="button" value=""  />
-            </div>
-            <div>
-                <input id="bt7" type="button" value="" />
-                <input id="bt8" type="button" value="" />
-                <input id="bt9" type="button" value="" />
-            </div>
-        </form>
-        <button id="btConsultLeadboard">Consult leadboard</button>
-        <div class="popup" id="popup">
-                <h2>Leaderboard</h2>
-                <table id="leaderboardTable">
-                    <thead>
-                        <tr>
-                            <th>Played at</th>
-                            <th>Result</th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-                <button id="btClose" type="button">
-                    OK
-                </button>
-            </div>
+    <div class="container">
+        <div class="zone">
+            <h1>Tic Tac Toe</h1>
+            <form>
+                <div>
+                    <input id="bt1" type="button" value=""  />
+                    <input id="bt2" type="button" value=""  />
+                    <input id="bt3" type="button" value=""  />
+                </div>
+                <div>
+                    <input id="bt4" type="button" value="" />
+                    <input id="bt5" type="button" value="" />
+                    <input id="bt6" type="button" value=""  />
+                </div>
+                <div>
+                    <input id="bt7" type="button" value="" />
+                    <input id="bt8" type="button" value="" />
+                    <input id="bt9" type="button" value="" />
+                </div>
+            </form>
+            <button id="btConsultLeadboard">Consult leadboard</button>
+            <div class="popup" id="popup">
+                    <h2>Leaderboard</h2>
+                    <table id="leaderboardTable">
+                        <thead>
+                            <tr>
+                                <th>Played at</th>
+                                <th>Result</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                    <button id="btClose" type="button">
+                        OK
+                    </button>
+                </div>
+        </div>
     </div>
-</div>
-<script src="{{ asset('assets/scripts/models/game.js') }}"></script>
-<script src="{{ asset('assets/scripts/script.js') }}"></script>
+    <script src="{{ asset('assets/scripts/models/game.js') }}"></script>
+    <script src="{{ asset('assets/scripts/script.js') }}"></script>
 @endsection
